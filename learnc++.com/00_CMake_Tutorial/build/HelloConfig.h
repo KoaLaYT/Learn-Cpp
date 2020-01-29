c9b3792c6f66f84cad3f01d3919357520a99cc85
KoaLaYT <hytohyeah@outlook.com>
2020-01-29 21:03:13 +0800

    .gitignore fix: remove build folders

--- learnc++.com/00_CMake_Tutorial/build/HelloConfig.h
+++
@@ -1,6 +0,0 @@
-// the configured options and settings for Hello
-
-#define Hello_VERSION_MAJOR 2
-#define Hello_VERSION_MINOR 3
-
-#define USE_MYMATH
--- learnc++.com/02_Functions_and_Files/Question03/build/Io/CMakeFiles/Io.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libIo.a"
-)
